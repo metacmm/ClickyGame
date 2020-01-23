0297afdd8b855355f1a7d84a06c638d6f7cfa5ea
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2020-01-22 23:20:49 -0800

    Updates

---
+++ precache-manifest.d39ca4cf997ba63f2334b14279985c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6de9ec7f7ccb3608f366b6c42a7f13e",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "f9eda3104826db663157",
+    "url": "/ClickyGame/static/css/main.8eedf0c6.chunk.css"
+  },
+  {
+    "revision": "72a4490dfb8e9609a67e",
+    "url": "/ClickyGame/static/js/2.747bbdea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.747bbdea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9eda3104826db663157",
+    "url": "/ClickyGame/static/js/main.36bf604b.chunk.js"
+  },
+  {
+    "revision": "88489d381a86dae2d822",
+    "url": "/ClickyGame/static/js/runtime-main.1386749d.js"
+  }
+]);

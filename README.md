199d8f12be3d949c485f92f9c649eb6a5d7d52d7
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2020-01-22 22:30:12 -0800

    add readme.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ClickyGame
+
+## Introduction
+
+Clicky Game is a React application where user's score is tracked. The user's score should be incremented when clicking an image for the first time. User's score should be reset to 0 if they click the same image more than once.
+
+Every time an image is clicked, the images rendered to the page should shuffle themselves in a random order. 
+
+Top score is remembered and will be reset to current score if current score is higher.
+
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,7 +6,7 @@ function Nav(props) {
         <nav className="navbar navbar-light fixed-top navtop" style={{ backgroundColor: "#e3f2fd" }}>
             <h1>Clicky Game</h1>
             <div className="justify-content-end">
-                <p>Score: {props.score} | Top Score: {props.topScore}</p>
+                <p>Score: {props.score/* comment*/} | Top Score: {props.topScore}</p>
                 <p className={props.guessed ? "textright" : "textwrong"}>You guessed {props.guessed ? "correctly!" : "incorrectly!"}</p>
             </div>
         </nav>
